d89607f6b1754ab5e62b6bd8035380f31832818a
Arian Farzanehnejad <afn2681@gmail.com>
2023-01-24 16:28:57 +0330

    login form

--- scripts/script.js
+++ scripts/script.js
@@ -1,24 +1,3 @@
-//5
+//6
 'use strict'
 
-    let ask=(question,yes,no)=>
-    {
-        if(confirm(question))yes()
-        else no()    
-    }
-    ask('do you agree?',
-    ()=>{alert("you agreed.")},
-    ()=>{alert("you canceled the execution");}
-)
-/*
-question:
-function ask(question,yes,no)
-{
-    if(confirm(question))yes()
-    else no()
-}
-ask('do you agree?',
-function(){alert("you agreed.")},
-function(){alert("you canceled the execution");}
-);
-*/
